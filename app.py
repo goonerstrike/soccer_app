aa3d3fe6c18d100b356c2f6484bf1117ef0bd0dd
ryan xte1 <test@cx-test01.sd.spawar.navy.mil>
2017-12-06 15:42:22 -0800

    basic working webapp with basic username Jackson

--- app.py
+++ app.py
@@ -1,24 +1,16 @@
-mport requests
-import urllib2
-import json
-
-
-###################################### football-data.org API
-# API Key 1310374de6494d8dbe31e7f84aa85f23
-
-
-
-# Function to retrieve all competitions
-def get_leagues():    
-    url = 'https://api.football-data.org/v1/competitions'
-    headers = {'Authorization': 'Bearer 1310374de6494d8dbe31e7f84aa85f23'}
-    r = requests.get(url, headers=headers, verify=False)
-    competitions = r.json()
-    return competitions
-
-epl = get_epl()
-epl_fixtures = epl["_links"]["fixtures"]
-
-for i in epl:
-    print i["caption"]
+from flask import Flask, flash, redirect, render_template, request, session, abort
+ 
+app = Flask(__name__)
+ 
+@app.route("/")
+def index():
+    return "Flask App!"
+ 
+@app.route("/hello/<string:name>/")
+def hello(name):
+    return render_template(
+        'test.html',name=name)
+ 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
 
--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,14 @@
 from flask import Flask
-application = Flask(__name__)
+app = Flask(__name__)
 
-@application.route("/")
+@app.route("/")
+def index():
+  return "Index!!!"
+
+@app.route("/hello")
 def hello():
-    return "Hello World!!!"
+  return "Hello World!!!"
+
 
 if __name__ == "__main__":
-    application.run()
+    app.run(host='0.0.0.0', port=80)
