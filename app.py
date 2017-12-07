d97c74c3f0e937a9ebdec779df1e197d0140d6da
ryan xte1 <test@cx-test01.sd.spawar.navy.mil>
2017-12-07 15:31:57 -0800

    working with loops through leagues and PL teams

--- app.py
+++ app.py
@@ -12,12 +12,17 @@ headers = {'Authorization': 'Bearer 1310374de6494d8dbe31e7f84aa85f23'}
 def index():
 	r = requests.get(comp_url, headers=headers, verify=False)
 	competitions = r.json()
-	leagues = []
+	league_name = []
+	league_acronym = []
 	for i in competitions:
-		leagues.append(i["caption"])
+		league_name.append(i["caption"])
+	for a in competitions:
+		league_acronym.append(a["league"])
+	leagues = zip(league_acronym, league_name)
 	return render_template('index.html', **locals())
+
  
-@app.route("/premierleague/")
+@app.route("/PL/")
 def epl():
 	url = 'https://api.football-data.org/v1/competitions/445/teams'
 	r = requests.get(url, headers=headers, verify=False)
@@ -25,7 +30,7 @@ def epl():
 	names = []
 	for i in teams["teams"]:
 		names.append(i["name"])
-	return render_template('/premierleague/index.html', names=names)
+	return render_template('/PL/index.html', names=names)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
