9fcce7f2ba3a0f057cd81bdec3a20a8da37c0f99
ryan xte1 <test@cx-test01.sd.spawar.navy.mil>
2017-12-07 10:05:40 -0800

    have basic leagues listed with soccerball image

--- app.py
+++ app.py
@@ -7,11 +7,6 @@ app = Flask(__name__)
  
 @app.route("/")
 def index():
-    return "Flask App!"
- 
-#@app.route("/hello/<string:name>")
-@app.route("/hello/<string:name>/")
-def hello(name):
 	url = 'https://api.football-data.org/v1/competitions'
 	headers = {'Authorization': 'Bearer 1310374de6494d8dbe31e7f84aa85f23'}
 	r = requests.get(url, headers=headers, verify=False)
@@ -19,7 +14,7 @@ def hello(name):
 	leagues = []
 	for i in competitions:
 		leagues.append(i["caption"])
-	return render_template('test.html', **locals())
+	return render_template('index.html', leagues=leagues)
  
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
