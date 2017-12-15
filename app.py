0d992be3e5f1b6a3fc53a40ff5005d382b7e0cb6
rch <root@cx-test01.sd.spawar.navy.mil>
2017-12-15 14:07:36 -0800

    adding app.py back in

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, flash, redirect, render_template, request, session, abort
+from random import randint
+import requests
+import json
+
+app = Flask(__name__)
+
+comp_url = 'https://api.football-data.org/v1/competitions'
+headers = {'Authorization': 'Bearer 1310374de6494d8dbe31e7f84aa85f23'}
+
+@app.route("/")
+def index():
+    r = requests.get(comp_url, headers=headers, verify=False)
+    competitions = r.json()
+    league_name = []
+    league_acronym = []
+    for i in competitions:
+        league_name.append(i["caption"])
+    for a in competitions:
+        league_acronym.append(a["league"])
+    leagues = zip(league_acronym, league_name)
+    return render_template('index.html', **locals())
+
+
+@app.route("/PL/")
+def epl():
+    url = 'https://api.football-data.org/v1/competitions/445/teams'
+    r = requests.get(url, headers=headers, verify=False)
+    squads = r.json()
+    team_name = []
+    team_logo = []
+    for i in squads["teams"]:
+        team_name.append(i["name"])
+        team_logo.append(i["crestUrl"])
+
+    return render_template('/PL/index.html', **locals())
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
