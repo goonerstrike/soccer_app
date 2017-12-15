ed0813bb2e13d782b76b3ea96ea078db7b56a6f4
rch <root@cx-test01.sd.spawar.navy.mil>
2017-12-15 15:15:35 -0800

    added images and sizing, as well as preliminary tables to EPL index

--- app.py
+++ app.py
@@ -10,31 +10,31 @@ headers = {'Authorization': 'Bearer 1310374de6494d8dbe31e7f84aa85f23'}
 
 @app.route("/")
 def index():
-    r = requests.get(comp_url, headers=headers, verify=False)
-    competitions = r.json()
-    league_name = []
-    league_acronym = []
-    for i in competitions:
-        league_name.append(i["caption"])
-    for a in competitions:
-        league_acronym.append(a["league"])
-    leagues = zip(league_acronym, league_name)
-    return render_template('index.html', **locals())
+  r = requests.get(comp_url, headers=headers, verify=False)
+  competitions = r.json()
+  league_name = []
+  league_acronym = []
+  for i in competitions:
+      league_name.append(i["caption"])
+  for a in competitions:
+      league_acronym.append(a["league"])
+  leagues = zip(league_acronym, league_name)
+  return render_template('index.html', **locals())
 
 
 @app.route("/PL/")
 def epl():
-    url = 'https://api.football-data.org/v1/competitions/445/teams'
-    r = requests.get(url, headers=headers, verify=False)
-    squads = r.json()
-    team_name = []
-    team_logo = []
-    for i in squads["teams"]:
-        team_name.append(i["name"])
-        team_logo.append(i["crestUrl"])
-
-    return render_template('/PL/index.html', **locals())
+  url = 'https://api.football-data.org/v1/competitions/445/teams'
+  r = requests.get(url, headers=headers, verify=False)
+  squads = r.json()
+  team_name = []
+  team_logo = []
+  for i in squads["teams"]:
+      team_name.append(i["shortName"])
+      team_logo.append(i["crestUrl"])
+      teams = zip(team_name, team_logo)
+  return render_template('/PL/index.html', **locals())
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+  app.run(host='0.0.0.0', port=80)
